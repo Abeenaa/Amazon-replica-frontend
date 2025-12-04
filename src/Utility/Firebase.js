cf5a6a6d94e96fda71fc8c836033ea4793279e24
Abeenaa <abeneendalew@gmail.com>
2025-12-04 20:52:57 +0300

    Add Firebase auth and login page

--- src/Utility/Firebase.js
+++ src/Utility/Firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // ←←← YOUR CONFIG (already correct)
 const firebaseConfig = {
-  apiKey: "AIzaSyAjn6DjEJAxLiHIrGkoDjjiYz1nr9qRe-o",
-  authDomain: "clone-a11e1.firebaseapp.com",
-  projectId: "clone-a11e1",
-  storageBucket: "clone-a11e1.firebasestorage.app",
-  messagingSenderId: "870885626044",
-  appId: "1:870885626044:web:d79741e938e698a3fbc0b9",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
